4f703967ae425787e2fd35d3e58c7dd9eb74a3b2
Yaaqoub Charkouk <yaaqoubcourses@gmail.com>
2025-12-06 15:10:25 +0100

    on the road to master java

---
+++ devAppJava/tp7.java
@@ -0,0 +1,38 @@
+
+
+
+
+class Etudiant {
+    String  nom;
+    int     age;
+    double  moyenne;
+
+// public :
+    public Etudiant(String n, int a, double m) {
+        this.nom = n;
+        this.age = a;
+        this.moyenne = m;
+    }
+    public void afficherDetails() {
+        System.out.println("Nom : " + this.nom);
+        System.out.println("Age : " + this.age);
+        System.out.println("Moyenne : " + this.moyenne);
+    }
+    public boolean estAdmis() {
+        return (this.moyenne >= 10);
+    }
+
+}
+
+public class tp7 {
+    public static void main(String[] args) {
+        Etudiant    etu = new Etudiant("Yaaqoub", 20, 19.75);
+        Etudiant    ishaq = new Etudiant("Ishaq", 18, 9.25);
+
+        etu.afficherDetails();
+        System.out.println(etu.estAdmis());
+
+        ishaq.afficherDetails();
+        System.out.println(ishaq.estAdmis());
+    }
+}
